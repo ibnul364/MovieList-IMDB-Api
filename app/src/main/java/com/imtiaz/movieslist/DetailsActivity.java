c9139f05bd58c8cd65703de4953943222f563b2d
ibnul364 <ayaz1873@gmail.com>
2022-02-02 13:22:17 +0600

    trailer showing in webview

--- app/src/main/java/com/imtiaz/movieslist/DetailsActivity.java
+++ app/src/main/java/com/imtiaz/movieslist/DetailsActivity.java
@@ -5,10 +5,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.imtiaz.movieslist.Listeners.OnDetailsApiListener;
 import com.imtiaz.movieslist.Model.MovieDetailsApiResponse;
@@ -17,12 +24,13 @@ import com.imtiaz.movieslist.Retrofit.ApiController;
 import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
-    TextView textView_movie_name,textView_movie_released_time,textView_movie_runtime,textView_movie_rating,textView_movie_votes,textView_movie_plot;
+    TextView textView_movie_name,textView_movie_released_time,textView_movie_runtime,textView_movie_rating,textView_movie_votes,textView_movie_plot,textView_movie_trailer_link;
     ImageView imageView_movie_poster;
     RecyclerView recycler_movie_cast;
     DetailsCastRecyclerAdapter adapter;
     ApiController apiController;
     ProgressDialog dialog;
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +43,9 @@ public class DetailsActivity extends AppCompatActivity {
         textView_movie_votes = findViewById(R.id.textView_movie_votes);
         textView_movie_plot = findViewById(R.id.textView_movie_plot);
         imageView_movie_poster = findViewById(R.id.imageView_movie_poster);
+        textView_movie_trailer_link = findViewById(R.id.textView_movie_trailer_link);
         recycler_movie_cast = findViewById(R.id.recycler_movie_cast);
+        webView = findViewById(R.id.webView);
 
         apiController = new ApiController(this);
 
@@ -70,11 +80,25 @@ public class DetailsActivity extends AppCompatActivity {
 
     private void showResults(MovieDetailsApiResponse response) {
         textView_movie_name.setText(response.getTitle());
-        textView_movie_released_time.setText("Released In: "+ response.getYear());
+        textView_movie_released_time.setText("In Theatre: "+ response.getYear());
         textView_movie_runtime.setText("Length: "+response.getLength());
         textView_movie_rating.setText("Rating: "+response.getRating());
         textView_movie_votes.setText(response.getRating_votes()+ " Votes");
         textView_movie_plot.setText(response.getPlot());
+        textView_movie_trailer_link.setText(response.getTrailer().getLink());
+
+        webView.setWebViewClient(new MyBrowser());
+        textView_movie_trailer_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               webView.getSettings().setLoadsImagesAutomatically(true);
+               webView.getSettings().setJavaScriptEnabled(true);
+               webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+               webView.loadUrl(response.getTrailer().getLink());
+            }
+        });
+
+
 
         try{
             Picasso.get().load(response.getPoster()).into(imageView_movie_poster);
@@ -87,5 +111,12 @@ public class DetailsActivity extends AppCompatActivity {
         adapter = new DetailsCastRecyclerAdapter(this,response.getCast());
         recycler_movie_cast.setAdapter(adapter);
 
+    }
+
+    private class MyBrowser extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
     }
 }
